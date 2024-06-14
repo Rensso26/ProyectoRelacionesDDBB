abb7d585872b1707866750ff5bfb02145d9ff1a5
Rensso26 <rnparra@uce.edu.ec>
2024-06-14 09:44:17 -0500

    limpieza de codigo

--- ProyectoRelaciones/src/main/java/uce/edu/ec/ProyectoRelacionesDDBB/repository/MateriasRepository.java
+++
@@ -1,7 +0,0 @@
-package uce.edu.ec.ProyectoRelacionesDDBB.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uce.edu.ec.ProyectoRelacionesDDBB.Entity.Materias;
-
-public interface MateriasRepository extends JpaRepository <Materias, String> {
-}
